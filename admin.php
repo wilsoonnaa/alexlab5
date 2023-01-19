bcb2b99989cc2b9ed4d7a17cee1eee8ac770a941
mesere <mesere@girleater.net>
2023-01-19 13:32:35 +0300

    typo

--- admin.php
+++ admin.php
@@ -2,6 +2,7 @@
     require("db.php");
     if (isset($_POST["submit"])) 
     {
+        $id = $_POST["id"];
         $name = $_POST["name"];
         $assignedTo = $_POST["assignedTo"];
         $deadline = $_POST["deadlines"];
@@ -24,7 +25,7 @@
 
 <body>
     
-    <form method="POST" action="create.php">
+    <form method="POST" action="admin.php">
         Task name: <input type="text" name="task_name" required /><br />
         Task solver: <input type="text" name="assignedTo" /><br />
         Deadline: <input type="date" name="deadline" /><br />
